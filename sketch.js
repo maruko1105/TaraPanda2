09d4f4015f47ac746200ab1af7d909bf03b9f01f
maruko1105 <maluko1105@gmail.com>
2015-12-03 18:16:50 -0500

    video

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var potSprite, lobsterSprite;
 var direction = 90; //circle initial direction moving down
 
 function setup() {
-  createCanvas(680,500);
+  createCanvas(650,450);
   pot=loadImage( "img/pot.png");
   lobster=loadImage("img/lobster.png");
   //create the sprites
